2a405f0bd9375fb11338c19725fa6a2c3e3430b5
xurmeinBackUps <wj.pavocrist@gmail.com>
2019-06-13 12:01:10 -0500

    toy_app_finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def hello
+        render html: "You can run but you can't hide, bitch"
+    end
 end
